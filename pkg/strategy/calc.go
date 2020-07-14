730d911b98affa8dad38e92b2d7a60753ee574f2
c9s <yoan@maicoin.com>
2020-07-14 22:33:47 +0800

    use symbol from the strategy config file

--- pkg/strategy/calc.go
+++ pkg/strategy/calc.go
@@ -21,7 +21,7 @@ func (c *VolumeCalculator) modifyBuyVolume(price float64) float64 {
 	maxChange := c.HistoricalHigh - c.HistoricalLow
 	pessimisticFactor := 0.1
 	targetPrice := c.HistoricalLow * (1 - pessimisticFactor) // we will get 1 at price 7500, and more below 7500
-	flatness := maxChange * 0.5                              // higher number buys more in the middle section. higher number gets more flat line, reduced to 0 at price 2000 * 10
+	flatness := maxChange * 0.3                             // higher number buys more in the middle section. higher number gets more flat line, reduced to 0 at price 2000 * 10
 	return math.Exp(-(price - targetPrice) / flatness)
 }
 
@@ -30,7 +30,7 @@ func (c *VolumeCalculator) modifySellVolume(price float64) float64 {
 	maxChange := c.HistoricalHigh - c.HistoricalLow
 	optimismFactor := 0.1
 	targetPrice := c.HistoricalHigh * (1 + optimismFactor) // target to sell most x1 at 10000.0
-	flatness := maxChange * 0.4                            // higher number sells more in the middle section, lower number sells fewer in the middle section.
+	flatness := maxChange * 0.22                           // higher number sells more in the middle section, lower number sells fewer in the middle section.
 	return math.Exp((price - targetPrice) / flatness)
 }
 
