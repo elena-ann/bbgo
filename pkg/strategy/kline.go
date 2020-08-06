e85921452dd3da83e3cdfb22e67aec2711f0402a
c9s <yoan@maicoin.com>
2020-08-06 13:19:53 +0800

    pass by reference so that we can cast interface

--- pkg/strategy/kline.go
+++ pkg/strategy/kline.go
@@ -123,14 +123,14 @@ func (strategy *KLineStrategy) OnKLineClosed(kline *types.KLine) {
 				(strategy.cache.IsTextFresh(reason, 30*time.Minute) &&
 					strategy.cache.IsObjectFresh(&detector, 10*time.Minute)) {
 
-				strategy.Notifier.Notify(trendIcon+" *SKIP* reason: %s", reason, detector, kline)
+				strategy.Notifier.Notify(trendIcon+" *SKIP* reason: %s", reason, &detector, kline)
 			}
 
 		} else {
 			if len(reason) > 0 {
-				strategy.Notifier.Notify(trendIcon+" *TRIGGERED* reason: %s", reason, detector, kline)
+				strategy.Notifier.Notify(trendIcon+" *TRIGGERED* reason: %s", reason, &detector, kline)
 			} else {
-				strategy.Notifier.Notify(trendIcon+" *TRIGGERED* ", detector, kline)
+				strategy.Notifier.Notify(trendIcon+" *TRIGGERED* ", &detector, kline)
 			}
 
 			var order, err = strategy.NewOrder(klineOrWindow, strategy.TradingContext)
